28f02b71e153ba3e1ab6fd2e8b29c84dc12782fd
zota <zota@example.com>
2023-10-08 11:27:09 +0900

    modif

--- cycloid_main.py
+++ cycloid_main.py
@@ -11,6 +11,9 @@ import pandas as pd
 import sys
 import time
 
+import matplotlib.style as mplstyle
+mplstyle.use('fast')
+
 def rotZ(th):
     return np.array([
         [ np.cos(th), np.sin(th), 0.0],
@@ -35,8 +38,8 @@ def drawPolyline(ax,poly,color='blue'):
         drawLine(ax,poly[i,0],poly[i,1],poly[i+1,0],poly[i+1,1],color=color)
 
 
-rc = 50
-rs = 25
+rc = 200
+rs = 100
 L = 100
 Lx = 55
 NUM = 100
@@ -54,54 +57,49 @@ def circle(r):
 
 class RollingCircle:
     def __init__(self,x,y,th):
-        #self.pxy = np.array([[rs],[0],[1]])
-        self.track = []
-        self.set(x,y,th)
+        
+        self.pxy_ini = np.array([[rs],[0],[1]])
+        self.track = self.pxy_ini * 1 ##copy to track
+        
+        #self.setPos(x,y,th)
+        
 
-    def set(self,x,y,th):
+    def setPos(self,x,y,th):
         self.x = x
         self.y = y
         self.th = th
         self.rotate = tr(x,y) @ rotZ(-th*3)
+        
+        self.c = self.rotate @ circle(rs)
+        self.pxy = self.rotate @ self.pxy_ini
+        
+        self.track = np.hstack((self.track, self.pxy))
 
-    def getTrack(self,ax,track):
-        return self.rotate @ track 
-    
     def dot(self,H):
         self.rotate = H @ self.rotate
     
     def draw(self,ax):
-        c = circle(rs)
-        line = np.array([[0,rs],[0,0],[1,1]])
-
-        c = self.rotate @ c
-        line = self.rotate @ line
-        ax.scatter(c[0],c[1],s=0.1)
-        ax.plot(line[0],line[1])
+        ax.scatter(self.c[0],self.c[1],s=0.1)
+        ax.scatter(self.pxy[0],self.pxy[1])
+        
+        
 
         #self.track.append(np.array(line[:,1]))
 
 view = tr(0,0)
-#track = np.array([[rs],[0],[1]])
-#track = []
 sc = RollingCircle(rc+rs,0,0)
+cc = circle(rc)
 
 for th in np.linspace(0,np.pi*2,NUM):
     fig,ax = plt.subplots(figsize=(8,8))
 
     x = (rc + rs) * np.cos(th)
     y = (rc + rs) * np.sin(th)
-    sc.set(x,y,th)
+    # print(f'{x},{y}')
+    sc.setPos(x,y,th)
 
     sc.draw(ax)
-#    sc.getTrack(ax,track)
-    # newpxy = sc.getTrack(ax,track[:,0])
-    # track = np.hstack((track,newpxy))
-    # ax.plot(track[0],track[1])
-
-    cc = circle(rc)
     ax.plot(cc[0],cc[1])
-
 
     ax.set_xlim([-500,500])
     ax.set_ylim([-500,500])
@@ -112,7 +110,7 @@ for th in np.linspace(0,np.pi*2,NUM):
     
     plt.clf()
     plt.close()
-#    break
+    # break
 
 
 
