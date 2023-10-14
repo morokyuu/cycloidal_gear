0144af427e4ef2db1b63790fa9e113e2c8424dd0
zota <zota@example.com>
2023-10-15 07:51:29 +0900

    offset?

--- cycloid_main.py
+++ cycloid_main.py
@@ -49,14 +49,17 @@ Lx = 55
 NUM = 100
 GRRANGE = 800
 TRACK_ENB = False
+TRACK_ENB = True
 SAVEFIG = False
 
 class RollingCircle:
     def __init__(self,th_ini):
         self.th_ini = th_ini
         self.RATIO = (rs+rc)*2*np.pi / (rs*2*np.pi)
-        self.pxy_ini = np.array([[rs*0.5],[0],[1]])
+        self.pxy_ini = np.array([[rs*0.7],[0],[1]])
+        self.pxy_ini2 = np.array([[rs*0.2],[0],[1]])
         self.track = np.zeros((3,1))
+        self.track2 = np.zeros((3,1))
 
     def revolve(self,th):
         return rotZ(-th-self.th_ini) @ tr(-rs+rc,0)
@@ -67,13 +70,17 @@ class RollingCircle:
     def setPos(self,th):
         self.xy = self.revolve(th) @ np.array([[0],[0],[1]])
         self.pxy = self.revolve(th) @ self.rotate(th) @ self.pxy_ini
+        self.pxy2 = self.revolve(th) @ self.rotate(th) @ self.pxy_ini2
         self.track = np.hstack((self.track, self.pxy))
+        self.track2 = np.hstack((self.track2, self.pxy2))
 
     def draw(self,ax):
         drawCircle(ax, self.xy[0], self.xy[1], rs)
         ax.scatter(self.pxy[0],self.pxy[1])
+        ax.scatter(self.pxy2[0],self.pxy2[1])
         if TRACK_ENB:
             ax.plot(self.track[0,1:],self.track[1,1:])
+            ax.plot(self.track2[0,1:],self.track2[1,1:])
 
 view = tr(0,0)
 
