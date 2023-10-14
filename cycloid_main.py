4f10181bd4ea6056f52a61ce67d35e5fd276dd70
zota <zota@example.com>
2023-10-15 08:18:19 +0900

    one small circle

--- cycloid_main.py
+++ cycloid_main.py
@@ -55,14 +55,13 @@ SAVEFIG = False
 class RollingCircle:
     def __init__(self,th_ini):
         self.th_ini = th_ini
+        #self.RATIO = (rs+rc)*2*np.pi / (rs*2*np.pi)
         self.RATIO = (rs+rc)*2*np.pi / (rs*2*np.pi)
-        self.pxy_ini = np.array([[rs*0.7],[0],[1]])
-        self.pxy_ini2 = np.array([[rs*0.2],[0],[1]])
+        self.pxy_ini = np.array([[rs*0.5],[0],[1]])
         self.track = np.zeros((3,1))
-        self.track2 = np.zeros((3,1))
 
     def revolve(self,th):
-        return rotZ(-th-self.th_ini) @ tr(-rs+rc,0)
+        return rotZ(th-self.th_ini) @ tr(rs+rc,0)
 
     def rotate(self,th):
         return rotZ(self.RATIO * (th+self.th_ini))
@@ -70,41 +69,38 @@ class RollingCircle:
     def setPos(self,th):
         self.xy = self.revolve(th) @ np.array([[0],[0],[1]])
         self.pxy = self.revolve(th) @ self.rotate(th) @ self.pxy_ini
-        self.pxy2 = self.revolve(th) @ self.rotate(th) @ self.pxy_ini2
         self.track = np.hstack((self.track, self.pxy))
-        self.track2 = np.hstack((self.track2, self.pxy2))
 
     def draw(self,ax):
         drawCircle(ax, self.xy[0], self.xy[1], rs)
         ax.scatter(self.pxy[0],self.pxy[1])
-        ax.scatter(self.pxy2[0],self.pxy2[1])
         if TRACK_ENB:
             ax.plot(self.track[0,1:],self.track[1,1:])
-            ax.plot(self.track2[0,1:],self.track2[1,1:])
 
 view = tr(0,0)
 
 sc = []
-for th_offs in np.linspace(0,2*np.pi,6):
-    sc.append(RollingCircle(th_offs))
+#for th_offs in np.linspace(0,2*np.pi,6):
+#    sc.append(RollingCircle(th_offs))
+sc.append(RollingCircle(0))
 
 num = 0
 for th in np.linspace(0,np.pi*2,NUM):
     fig,ax = plt.subplots(figsize=(8,8))
 
-    poly = np.array([[0],[0],[1]])
+#    poly = np.array([[0],[0],[1]])
     for s in sc:
         s.setPos(th)
         s.draw(ax)
-        poly = np.hstack((poly,s.pxy))
+#        poly = np.hstack((poly,s.pxy))
     
     drawCircle(ax, 0,0, rc)
 
-    drawPolyline(ax,poly[:,1:])
+    # drawPolyline(ax,poly[:,1:])
     
-    ecce = rotZ(((rc-rs)/rs+1) * th) @ sc[0].pxy_ini
-    ecce = np.hstack((np.zeros((3,1)),ecce))
-    ax.scatter(ecce[0],ecce[1])
+#    ecce = rotZ(((rc-rs)/rs+1) * th) @ sc[0].pxy_ini
+#    ecce = np.hstack((np.zeros((3,1)),ecce))
+#    ax.scatter(ecce[0],ecce[1])
 
     ax.set_xlim([-GRRANGE,GRRANGE])
     ax.set_ylim([-GRRANGE,GRRANGE])
