10a586de20c3c79d714c6284fd3ce64f4cdb916d
zota <zota@example.com>
2023-10-15 09:37:40 +0900

    only track drawing

--- cycloid_main.py
+++ cycloid_main.py
@@ -55,7 +55,6 @@ SAVEFIG = False
 class RollingCircle:
     def __init__(self,th_ini):
         self.th_ini = th_ini
-        #self.RATIO = (rs+rc)*2*np.pi / (rs*2*np.pi)
         self.RATIO = (rs+rc)*2*np.pi / (rs*2*np.pi)
         self.pxy_ini = np.array([[rs*0.5],[0],[1]])
         self.track = np.zeros((3,1))
@@ -69,13 +68,19 @@ class RollingCircle:
     def setPos(self,th):
         self.xy = self.revolve(th) @ np.array([[0],[0],[1]])
         self.pxy = self.revolve(th) @ self.rotate(th) @ self.pxy_ini
-        self.track = np.hstack((self.track, self.pxy))
+        #self.track = np.hstack((self.track, self.pxy))
+
+    def getTrack(self):
+        for th in np.linspace(0,2*np.pi,NUM):
+            pxy = self.revolve(th) @ self.rotate(th) @ self.pxy_ini
+            self.track = np.hstack((self.track, pxy))
+        return self.track
 
     def draw(self,ax):
         drawCircle(ax, self.xy[0], self.xy[1], rs)
         ax.scatter(self.pxy[0],self.pxy[1])
-        if TRACK_ENB:
-            ax.plot(self.track[0,1:],self.track[1,1:])
+#        if TRACK_ENB:
+#            ax.plot(self.track[0,1:],self.track[1,1:])
 
 view = tr(0,0)
 
@@ -83,6 +88,19 @@ sc = []
 #for th_offs in np.linspace(0,2*np.pi,6):
 #    sc.append(RollingCircle(th_offs))
 sc.append(RollingCircle(0))
+
+
+######
+fig,ax = plt.subplots(figsize=(8,8))
+track = sc[0].getTrack()
+ax.plot(track[0,1:],track[1,1:])
+drawCircle(ax,0,0, rc)
+ax.set_xlim([-GRRANGE,GRRANGE])
+ax.set_ylim([-GRRANGE,GRRANGE])
+ax.set_aspect('equal')
+ax.grid()
+sys.exit(0)
+######
 
 num = 0
 for th in np.linspace(0,np.pi*2,NUM):
